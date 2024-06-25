98385374137feb3cbf4579f2238ffc550afc5a35
Augustinho <augustinhoo01@hotmail.com>
2024-06-25 14:54:58 -0300

    Lista encadeada get(NO)

--- ListaEncadeada/src/one/digitalinnovation/ListaEncadeada.java
+++ ListaEncadeada/src/one/digitalinnovation/ListaEncadeada.java
@@ -8,6 +8,25 @@ public class ListaEncadeada<T> {
 		this.referenciaEntrada = null;
 	}
 	
+	private No<T> getNo(int index){
+		
+		validaIndice(index);
+		
+		if(index >= size()) {
+			int ultimoIndice = size() -1;
+			throw new IndexOutOfBoundsException("Não existe conteúdo no indice " + index + " desta lista. Esta lista só vai até o indice " + ultimoIndice + '.');
+		}
+		
+		No<T> noAuxiliar = referenciaEntrada;
+		No<T> noRetorno = null;
+		
+		for(int i = 0; i < this.size()-1; i++) {
+			noRetorno = noAuxiliar;
+			noAuxiliar = noAuxiliar.getProximoNo();
+		}
+		return noRetorno;
+	}
+	
 	public int size(){
 		int tamanhoLista = 0;
 		
@@ -25,6 +44,13 @@ public class ListaEncadeada<T> {
 		}
 		
 		return tamanhoLista;
+	}
+	
+	private void validaIndice(int index) {
+		if(index >= size()) {
+		int ultimoIndice = size() -1;
+		throw new IndexOutOfBoundsException("Não existe conteúdo no indice " + index + " desta lista. Esta lista só vai até o indice " + ultimoIndice + '.');
+	}
 	}
 	
 	public boolean isEmpty() {
